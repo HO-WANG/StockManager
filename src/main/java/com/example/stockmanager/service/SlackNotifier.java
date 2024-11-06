c07b2b3d899e4e0f35344773130208eeb5eae513
jihong <parkatrix@gmail.com>
2024-11-07 00:34:57 +0900

    슬랙 웹훅 주소 변경

--- src/main/java/com/example/stockmanager/service/SlackNotifier.java
+++ src/main/java/com/example/stockmanager/service/SlackNotifier.java
@@ -9,7 +9,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class SlackNotifier {
 
     private final WebClient webClient;
-    private final String webhookUrl = "https://hooks.slack.com/services/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";
+    private final String webhookUrl = "https://hooks.slack.com/services/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";
 
     public void sendNotification(String message) {
         webClient.post()
